40d891b4d760fe648ca948d991ec997032be6958
user01 <ezheng@arizona.edu>
2025-06-19 09:49:02 -0400

    .

--- train_vision.py
+++ train_vision.py
@@ -95,7 +95,7 @@ def train(
     logger.info("Preparing dataset …")
 
     # collate function with teacher forcing during warmup
-    collate_fn = make_collate_fn(tokenizer.pad_token_id, tokenizer, total_steps=warmup_ratio*epochs*steps_per_epoch, tf_start_p=1, tf_end_p=0, schedule="linear", logger=logger, logger_steps= max(batch_size // grad_acc_steps, 1))
+    collate_fn = make_collate_fn(tokenizer.pad_token_id, tokenizer, total_steps=warmup_ratio*epochs*steps_per_epoch, tf_start_p=1, tf_end_p=0, schedule="linear", logger=logger, log_interval=max(batch_size // grad_acc_steps, 1))
 
     # Composite loss callable
     compute_loss_fn = create_compute_loss_func(tokenizer, logger=logger, log_interval = max(batch_size // grad_acc_steps, 1), lm_target=3)
